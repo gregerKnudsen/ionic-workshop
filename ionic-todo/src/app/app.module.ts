d5a9d795bd61cd98b1901feed5e6be351ff06a39
Greger Siem Knudsen <greger_knudsen@hotmail.com>
2017-06-28 21:17:02 +0200

    Step four completed

--- ionic-todo/src/app/app.module.ts
+++ ionic-todo/src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { IonicStorageModule } from '@ionic/Storage';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { AddItemPage } from '../pages/add-item/add-item';
 import { ItemDetailPage } from '../pages/item-detail/item-detail';
+import { Data } from '../providers/data';
 
 @NgModule({
   declarations: [
@@ -17,7 +19,8 @@ import { ItemDetailPage } from '../pages/item-detail/item-detail';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot()
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -27,6 +30,7 @@ import { ItemDetailPage } from '../pages/item-detail/item-detail';
     ItemDetailPage
   ],
   providers: [
+    Data,
     StatusBar,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
